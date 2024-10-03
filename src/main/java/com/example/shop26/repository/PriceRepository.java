33a56f85e8dd585aa6a363ac5dfa80499632480d
Totokh <tonytruanth@gmail.com>
2024-10-03 11:59:15 +0000

    feat: MR with api+unit tests+integration test+ swagger access at http://localhost:8080/swagger-ui/index.html#/

--- src/main/java/com/example/shop26/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop26.repository;
-
-import com.example.shop26.model.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository extends JpaRepository<Price, Long> {
-}
--- src/main/java/com/example/shop26/repository/QuantityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop26.repository;
-
-import com.example.shop26.model.Quantity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuantityRepository extends JpaRepository<Quantity, Long> {
-}
