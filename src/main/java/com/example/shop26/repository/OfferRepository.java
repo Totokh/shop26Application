a25c7ec795fe72d8bad60deb3aacee240f6b1fbe
Totokh <tonytruanth@gmail.com>
2024-10-03 11:57:29 +0000

    Setup MR with all development

--- src/main/java/com/example/shop26/repository/OfferRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shop26.repository;
-
-import com.example.shop26.model.Offer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferRepository extends JpaRepository<Offer, Long> {
-    Offer findByProduct_ProductId(Long productId);
-}
--- src/main/java/com/example/shop26/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop26.repository;
-
-import com.example.shop26.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
